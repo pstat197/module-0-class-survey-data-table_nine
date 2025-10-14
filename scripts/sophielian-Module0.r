8bc2c8b29cfe601203a284c38afa502fef14bac1
Sophie Lian <sophielian@ucsb.edu>
2025-10-14 16:21:11 -0700

    ANOVA assumptions

--- scripts/sophielian-Module0.r
+++ scripts/sophielian-Module0.r
@@ -28,16 +28,51 @@ long_data <- anova_data %>%
 aov_result <- aov(diff ~ domain, data = long_data)
 summary(aov_result)
 
+# Interpretation of results
+
 TukeyHSD(aov_result)
+
+# Interpretation of finding which two domain means differ from one another
+
+
+
+
+
 
 #ANOVA Assumption Checking
 
-# Normality
-qqnorm()
-qqline()
+# Normality:
 
-# QQ plot
+qqnorm(aov_result$residuals)
+qqline(aov_result$residuals)
+
+# Looking at the qqnorm() plot, we can see that the plotted residual points seem
+# to fall far from the normal line towards the tails of the graph. Thus, we are 
+# skeptical whether the assumption of normality is met.
+
+
+shapiro.test(aov_result$residuals)
+
+# A Shapiro–Wilk test on the ANOVA residuals indicated that the assumption of 
+# normality was violated (W = 0.963, p < 0.001). This suggests that the residuals 
+# deviate significantly from a normal distribution. However, since ANOVA is robust 
+# to moderate non-normality with sufficiently large samples, the results can 
+# still be interpreted with caution.
+
+
+
+
 
 # Residuals vs. Fitted plot
 
-# interpret values
+plot(aov_result$residuals ~ aov_result$fitted.values)
+
+# In this plot of residuals versus fitted values, we have vertical columns of 
+# data points that correspond to the different treatment groups in this model. 
+# Within each group, we can observe that the residuals are approximately scattered 
+# around 0. This indicates that they have approximately equal variances, 
+# satisfying the ANOVA assumption.
+
+
+
+
