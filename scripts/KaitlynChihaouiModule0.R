3a7eb6c28a2e8b9b070ff1c5fe6dab8c87690163
KaitlynChihaoui <ktchihaoui@umail.ucsb.edu>
2025-10-14 14:55:27 -0700

    standardized again

--- scripts/KaitlynChihaouiModule0.R
+++ scripts/KaitlynChihaouiModule0.R
@@ -13,6 +13,14 @@ t_stat <- t.test(background$diff.stat, mu = 0)
 t_prog <- t.test(background$diff.prog, mu = 0)
 
 
+#t-tests one-to-one for each mean
+t_math_to_stat <- t.test(background$diff.math, background$diff.stat)
+t_stat_to_prog <- t.test(background$diff.stat, background$diff.prog)
+t_prog_to_math <- t.test(background$diff.prog, background$diff.math)
+
+library(knitr)
+kable()
+
 library(knitr)
 kable(t_results, digits = 3, caption = 
         "One-sample t-tests for comfort–proficiency differences by domain")
@@ -38,5 +46,24 @@ summary(anova_model)
 
 
 TukeyHSD(anova_model)
+
+# Standardizing Process
+prog_diff <- background$prog.prof - background$prog.comf
+math_diff <- background$math.prof - background$math.comf
+stat_diff <- background$stat.prof - background$stat.comf
+
+prog_diff_standardized <- scale(prog_diff)
+math_diff_standardized <- scale(math_diff)
+stat_diff_standardized <- scale(stat_diff)
+
+#ANOVA Assumption Checking
+
+# Normality
+qqnorm()
+qqline()
+
+# QQ plot
+
+# Residuals vs. Fitted plot
 
 # interpret values
